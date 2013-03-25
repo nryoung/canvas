9854f9148c9bf3a9850372640cca9f66b9299d87
Nic Young <nryoung@gmail.com>
2013-03-24 22:32:19 -0600

    Example 2.13 done.

---
+++ ch2/example2_13.js
@@ -0,0 +1,27 @@
+var canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+
+// Functions
+
+function drawGrid(context, color, stepx, stepy) {
+    context.strokeStyle = color;
+    context.lineWidth = 0.5;
+
+    for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
+        context.beginPath();
+        context.moveTo(i, 0);
+        context.lineTo(i, context.canvas.height);
+        context.stroke();
+    }
+
+    for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
+        context.beginPath();
+        context.moveTo(0, i);
+        context.lineTo(context.canvas.width, i);
+        context.stroke();
+    }
+}
+
+// Init
+
+drawGrid(context, 'lightgray', 10, 10);
